d079316b2f8ed037d3fd4b2029e923fead83dcfc
sorrel012 <wain1719@gmail.com>
2023-07-31 01:31:11 +0900

    feat: Study components

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,29 +1,47 @@
 <template>
 
+  <ModalView/>
+
   <div class="menu">
     <!-- <a v-for="i in navbar" :key="i">{{ i }}</a> -->
-    <a v-for="(a,i) in navbar" :key="i">{{ i }}</a>
+    <a v-for="(a,i) in navbar" :key="i">{{ a }}</a>
   </div>
 
-  <div v-for="(a,i) in products" :key="i">
+  <DiscountBanner/>  
+
+  <!-- <div v-for="(a,i) in products" :key="i">
     <img :src="require('./assets/room' + (i+1) + '.jpg')">
     <h4 >{{ a }}</h4>
     <p>{{ price[i] }}만원</p>
     <button @click="increase(i)">신고하기</button> <span>신고수: {{ reports[i] }}</span>
+  </div> -->
+
+  <div v-for="(a,i) in rooms" :key="i">
+    <img :src="a.image" class="images">
+    <h4 @click="isOpened = true; isClicked = i " class="titleModal">{{ a.title }}</h4>
+    <p>{{ a.price }}원</p>
   </div>
+
 </template>
 
 <script>  
+
+  import data from './assets/oneroom.js';
+  import DiscountBanner from './DiscountBanner.vue';
+  import ModalView from './ModalView.vue';
 
   export default {
     name: 'App',
     data() {
       ///데이터 보관함(object 자료로)
-      return {
+      return {        
         price: [40, 45],
         products: ['자연빌', 'cs타워'],
         navbar: ['Home', 'Shop', 'About'],
-        reports: [0, 0]
+        reports: [0, 0],
+        isOpened: false,
+        rooms: data,
+        isClicked: 0
       }
     },
     methods: {
@@ -32,6 +50,8 @@
       }
     },
     components: {
+      DiscountBanner : DiscountBanner,
+      ModalView : ModalView,
     }
   }
     
@@ -60,6 +80,37 @@
 
   img {
     margin-top: 40px;;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  div {
+    box-sizing: border-box;
+  }
+
+  .bg1 {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    position: fixed;
+    padding: 20px;
+  }
+
+  .bg2 {
+    width: 100%;
+    background-color: white;
+    border-radius: 8px;
+    padding: 20px;
+  }
+
+  .images {
+    width: 60%;
+  }
+
+  .titleModal:hover {
+    cursor: pointer;
   }
 
 </style>
---
+++ vuedongsan/src/ModalView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg1" v-if="isOpened == true">
+    <div class="bg2">
+     <img :src="rooms[isClicked].image" class="images">
+      <h3>{{ rooms[isClicked].title }}</h3>
+      <h4>{{ rooms[isClicked].content }}</h4>
+      <p>{{ rooms[isClicked].price }}원</p>
+      <button @click="isOpened=false">닫기</button>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+    name: 'ModalView',
+}
+</script>
+  
+<style>
+</style>
