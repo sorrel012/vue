adce79baf8c0a9012955dd742de6580cb354907f
sorrel012 <wain1719@gmail.com>
2023-08-03 15:04:16 +0900

    feat: Update projects

--- blog/src/assets/blog.js
+++ blog/src/assets/blog.js
@@ -2,17 +2,17 @@ export default [
     { 
       title : '첫 번째 프로젝트 : 허위매물 전문 부동산 앱',
       content : 'Vue를 이용하면 비누같이 매끈한 앱을 만들 수 있습니다',
-      date : 'September 24, 2021',
+      date : 'July 24, 2023',
       number : 0
     },{ 
       title : '두 번째 프로젝트 : 오마카세 배달 앱',
       content : '음식이 아니라 셰프를 직접 배달해드립니다',
-      date : 'October 20, 2020',
+      date : 'October 20, 2022',
       number : 1
     },{ 
-      title : '세 번째 프로젝트 : 현피 앱',
-      content : '거리를 설정하면 가장 가까운 파이터를 소개해드려요! 서로 싸워보세요',
-      date : 'April 24, 2019',
+      title : '세 번째 프로젝트 : 대중교통 앱',
+      content : '출발지와 도착지를 설정하면 가장 빠른 경로를 소개해드려요!',
+      date : 'April 24, 2011',
       number : 2
     }
   ]
